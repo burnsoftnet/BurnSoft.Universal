b215147b6e8b3a07bf3bdc2037249219afa19061
Joe Mireles <joe.mireles@atlasied.com>
2018-12-11 15:19:34 -0500

    #2 Created Unit Test Project

---
+++ UnitTest_BurnSoftUniversal/General.vb
@@ -0,0 +1,36 @@
+﻿
+<TestClass()> Public Class General
+    Public Shared Sub HasValue(sValue As String,Optional errMsg As String = "")
+        Dim didPass As Boolean = false
+        If (errMsg.Length > 0)
+            Debug.Print("ERROR!!")
+            Debug.Print(errMsg)
+        Else 
+            If (sValue.Length > 0)
+                Debug.Print(sValue)
+                didPass = True
+            End If
+        End If
+        Assert.AreEqual(didPass, True)
+    End Sub
+
+    Public Shared Sub HasValue(lValue As Long,Optional zeroOk As Boolean = false, Optional errMsg As String = "")
+        Dim didPass as Boolean = false
+        if Not zeroOk Then
+            If (lValue > 0) Then
+                didPass = True
+                Debug.Print("{0}",lValue)
+            Else 
+                Debug.Print("Value is Zero!!")
+            End If
+        Else 
+            didPass = True
+            Debug.Print("{0}",lValue)
+        End If
+        Assert.AreEqual(didPass, True)
+    End Sub
+
+    Public Shared Sub HasValue (bValue As Boolean, Optional errMsg As String = "")
+        Assert.AreEqual(bValue, True)
+    End Sub
+End Class
---
+++ UnitTest_BurnSoftUniversal/Settings.vb
@@ -0,0 +1,5 @@
+﻿<TestClass()> Public Class Settings
+    Public Const ProcessName = "Dropbox"
+    Public Const CommandLineSearch = "type"
+    Public Const REG_SUBKEY = "Test"
+End Class
