dd57f3372a59e12f4a51be2b8c480a836b69e609
Joe Mireles <joe.mireles@atlasied.com>
2018-12-11 15:30:50 -0500

    #1 Updated Comments and Naming Convenstion

--- BurnSoft.Universal/BSProcessInfo.vb
+++ BurnSoft.Universal/BSProcessInfo.vb
@@ -3,6 +3,7 @@
 ' www.burnsoft.net
 '-------------------------------------------
 Imports System.Management
+' ReSharper disable once InconsistentNaming
 Public Class BSProcessInfo
     ''' <summary>
     ''' Simple WMI Call just pass the Object then the desciption of the object to look for
@@ -17,9 +18,15 @@ Public Class BSProcessInfo
         Obj = Nothing
         Return sAns
     End Function
-    Private Function GetProcessInfoByPID(PID As String, sObject As String) As String
+    ''' <summary>
+    ''' Gets the process information by pid.
+    ''' </summary>
+    ''' <param name="pid">The pid.</param>
+    ''' <param name="sObject">The s object.</param>
+    ''' <returns>System.String.</returns>
+    Private Function GetProcessInfoByPID(pid As String, sObject As String) As String
         Dim sAns As String = ""
-        Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE ProcessId=" & PID)
+        Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE ProcessId=" & pid)
 
         For Each process As ManagementObject In searcher.Get()
             sAns = process(sObject)
@@ -30,69 +37,69 @@ Public Class BSProcessInfo
     ''' <summary>
     ''' Get the handlecount for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>Count</returns>
-    Public Function GetProccessHandleCount(PID As String) As String
-        Return GetProcessInfoByPID(PID, "HandleCount")
+    Public Function GetProccessHandleCount(pid As String) As String
+        Return GetProcessInfoByPID(pid, "HandleCount")
     End Function
     ''' <summary>
     ''' Get the ThreadCount for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>Count</returns>
-    Public Function GetProcessThreadCount(PID As String) As String
-        Return GetProcessInfoByPID(PID, "ThreadCount")
+    Public Function GetProcessThreadCount(pid As String) As String
+        Return GetProcessInfoByPID(pid, "ThreadCount")
     End Function
     ''' <summary>
     ''' Get the TerminationDate for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>Termination Date</returns>
-    Public Function GetProcessTerminationDate(PID As String) As String
-        Return GetProcessInfoByPID(PID, "TerminationDate")
+    Public Function GetProcessTerminationDate(pid As String) As String
+        Return GetProcessInfoByPID(pid, "TerminationDate")
     End Function
     ''' <summary>
     ''' Get the Caption for a particular Process by PID
     ''' Short description of an object—a one-line string.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>string</returns>
-    Public Function GetProcessCaption(PID As String) As String
-        Return GetProcessInfoByPID(PID, "Caption")
+    Public Function GetProcessCaption(pid As String) As String
+        Return GetProcessInfoByPID(pid, "Caption")
     End Function
     ''' <summary>
     ''' Get the CommandLine for a particular Process by PID
     ''' Command line used to start a specific process, if applicable.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>string</returns>
-    Public Function GetProcessCommandLine(PID As String) As String
-        Return GetProcessInfoByPID(PID, "CommandLine")
+    Public Function GetProcessCommandLine(pid As String) As String
+        Return GetProcessInfoByPID(pid, "CommandLine")
     End Function
     ''' <summary>
     ''' Get the CreationDate for a particular Process by PID
     ''' Date the process begins executing.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>datetime as string</returns>
-    Public Function GetProcessCreationDate(PID As String) As String
-        Return GetProcessInfoByPID(PID, "CreationDate")
+    Public Function GetProcessCreationDate(pid As String) As String
+        Return GetProcessInfoByPID(pid, "CreationDate")
     End Function
     ''' <summary>
     ''' Get the Description for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <returns>Description of an object.</returns>
-    Public Function GetProcessDescription(PID As String) As String
-        Return GetProcessInfoByPID(PID, "Description")
+    Public Function GetProcessDescription(pid As String) As String
+        Return GetProcessInfoByPID(pid, "Description")
     End Function
     ''' <summary>
     ''' Get the ExecutablePath for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Path to the executable file of the process.</return>
-    Public Function GetProcessExecutablePath(PID As String) As String
-        Return GetProcessInfoByPID(PID, "ExecutablePath")
+    Public Function GetProcessExecutablePath(pid As String) As String
+        Return GetProcessInfoByPID(pid, "ExecutablePath")
     End Function
     ''' <summary>
     ''' Get the ExecutionState for a particular Process by PID
@@ -107,28 +114,28 @@ Public Class BSProcessInfo
     ''' Stopped (8)
     ''' Growing (9)
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Current operating condition of the process</return>
-    Public Function GetProcessExecutionState(PID As String) As String
-        Return GetProcessInfoByPID(PID, "ExecutionState")
+    Public Function GetProcessExecutionState(pid As String) As String
+        Return GetProcessInfoByPID(pid, "ExecutionState")
     End Function
     ''' <summary>
     ''' Get the PageFaults for a particular Process by PID
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Number of page faults that a process generates.</return>
-    Public Function GetProcessPageFaults(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PageFaults")
+    Public Function GetProcessPageFaults(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PageFaults")
     End Function
     ''' <summary>
     ''' Get the PageFileUsage for a particular Process by PID
     ''' Amount of page file space that a process is using currently. 
     ''' This value is consistent with the VMSize value in TaskMgr.exe.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("kilobytes")</return>
-    Public Function GetProcessPageFileUsage(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PageFileUsage")
+    Public Function GetProcessPageFileUsage(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PageFileUsage")
     End Function
     ''' <summary>
     ''' Get the ParentProcessId for a particular Process by PID
@@ -141,19 +148,19 @@ Public Class BSProcessInfo
     ''' CreationDate property to determine whether the specified parent was 
     ''' created after the process represented by this Win32_Process instance was created
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Parent Process Id</return>
-    Public Function GetProcessParentProcessId(PID As String) As String
-        Return GetProcessInfoByPID(PID, "ParentProcessId")
+    Public Function GetProcessParentProcessId(pid As String) As String
+        Return GetProcessInfoByPID(pid, "ParentProcessId")
     End Function
     ''' <summary>
     ''' Get the PeakPageFileUsage for a particular Process by PID
     ''' Maximum amount of page file space used during the life of a process.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("kilobytes")</return>
-    Public Function GetProcessPeakPageFileUsage(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PeakPageFileUsage")
+    Public Function GetProcessPeakPageFileUsage(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PeakPageFileUsage")
     End Function
     ''' <summary>
     ''' Get the PeakVirtualSize for a particular Process by PID
@@ -163,29 +170,29 @@ Public Class BSProcessInfo
     ''' finite, and by using too much the process might not be able to load 
     ''' libraries.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("bytes")</return>
-    Public Function GetProcessPeakVirtualSize(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PeakVirtualSize")
+    Public Function GetProcessPeakVirtualSize(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PeakVirtualSize")
     End Function
     ''' <summary>
     ''' Get the PeakWorkingSetSize for a particular Process by PID
     ''' Peak working set size of a process.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("kilobytes")</return>
-    Public Function GetProcessPeakWorkingSetSize(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PeakWorkingSetSize")
+    Public Function GetProcessPeakWorkingSetSize(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PeakWorkingSetSize")
     End Function
     ''' <summary>
     ''' Get the PrivatePageCount for a particular Process by PID
     ''' Current number of pages allocated that are only accessible to the process 
     ''' represented by this Win32_Process instance.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>count</return>
-    Public Function GetProcessPrivatePageCount(PID As String) As String
-        Return GetProcessInfoByPID(PID, "PrivatePageCount")
+    Public Function GetProcessPrivatePageCount(pid As String) As String
+        Return GetProcessInfoByPID(pid, "PrivatePageCount")
     End Function
     ''' <summary>
     ''' Get the SessionId for a particular Process by PID
@@ -193,20 +200,20 @@ Public Class BSProcessInfo
     ''' session is created. A session spans a period of time from logon 
     ''' until logoff from a specific system.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>sessionID</return>
-    Public Function GetProcessSessionId(PID As String) As String
-        Return GetProcessInfoByPID(PID, "SessionId")
+    Public Function GetProcessSessionId(pid As String) As String
+        Return GetProcessInfoByPID(pid, "SessionId")
     End Function
     ''' <summary>
     ''' Get the UserModeTime for a particular Process by PID
     ''' Time in user mode, in 100 nanosecond units. If this information 
     ''' is not available, use a value of 0 (zero).
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("100 nanoseconds")</return>
-    Public Function GetProcessUserModeTime(PID As String) As String
-        Return GetProcessInfoByPID(PID, "UserModeTime")
+    Public Function GetProcessUserModeTime(pid As String) As String
+        Return GetProcessInfoByPID(pid, "UserModeTime")
     End Function
     ''' <summary>
     ''' Get the VirtualSize for a particular Process by PID
@@ -217,10 +224,10 @@ Public Class BSProcessInfo
     ''' process might not be able to load libraries. This value is consistent with 
     ''' what you see in Perfmon.exe.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("bytes")</return>
-    Public Function GetProcessVirtualSize(PID As String) As String
-        Return GetProcessInfoByPID(PID, "VirtualSize")
+    Public Function GetProcessVirtualSize(pid As String) As String
+        Return GetProcessInfoByPID(pid, "VirtualSize")
     End Function
     ''' <summary>
     ''' Get the WorkingSetSize for a particular Process by PID
@@ -231,28 +238,28 @@ Public Class BSProcessInfo
     ''' set data is provided, you can monitor the information to understand the changing 
     ''' memory requirements of a process.
     ''' </summary>
-    ''' <param name="PID"></param>
+    ''' <param name="pid"></param>
     ''' <return>Units ("bytes")</return>
-    Public Function GetProcessWorkingSetSize(PID As String) As String
-        Return GetProcessInfoByPID(PID, "WorkingSetSize")
+    Public Function GetProcessWorkingSetSize(pid As String) As String
+        Return GetProcessInfoByPID(pid, "WorkingSetSize")
     End Function
 
     ''' <summary>
     ''' Check to see if a process exists by name, Optionally get the PID and Process Count for the result(s)
     ''' </summary>
-    ''' <param name="ProcessName"></param>
-    ''' <param name="PID"></param>
-    ''' <param name="ProcessCount"></param>
+    ''' <param name="processName"></param>
+    ''' <param name="pid"></param>
+    ''' <param name="processCount"></param>
     ''' <returns></returns>
-    Public Function ProcessExists(ProcessName As String, Optional ByRef PID As String = "", Optional ByRef ProcessCount As Integer = 0) As Boolean
+    Public Function ProcessExists(processName As String, Optional ByRef pid As String = "", Optional ByRef processCount As Integer = 0) As Boolean
         Dim bAns As Boolean = False
         Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE Name like '" &
-                                                     ProcessName & "%'")
+                                                     processName & "%'")
 
         For Each process As ManagementObject In searcher.Get()
             bAns = True
             ProcessCount += 1
-            PID &= process("ProcessId")
+            pid &= process("ProcessId")
         Next
         searcher = Nothing
         Return bAns
@@ -260,21 +267,21 @@ Public Class BSProcessInfo
     ''' <summary>
     ''' Check to see if a process exists by name and what might be in the commandline parameters, Optionally get the PID and Process Count for the result(s)
     ''' </summary>
-    ''' <param name="ProcessName"></param>
-    ''' <param name="CommandLineContains"></param>
-    ''' <param name="PID"></param>
-    ''' <param name="ProcessCount"></param>
+    ''' <param name="processName"></param>
+    ''' <param name="commandLineContains"></param>
+    ''' <param name="pid"></param>
+    ''' <param name="processCount"></param>
     ''' <returns></returns>
-    Public Function ProcessExists(ProcessName As String, CommandLineContains As String, Optional ByRef PID As String = "", Optional ByRef ProcessCount As Integer = 0) As Boolean
+    Public Function ProcessExists(processName As String, commandLineContains As String, Optional ByRef pid As String = "", Optional ByRef processCount As Integer = 0) As Boolean
         Dim bAns As Boolean = False
         Dim searcher As New ManagementObjectSearcher("SELECT * FROM Win32_Process WHERE Name like '" &
                                                      ProcessName & "%' and CommandLine Like '%" &
-                                                     CommandLineContains & "%'")
+                                                     commandLineContains & "%'")
 
         For Each process As ManagementObject In searcher.Get()
             bAns = True
-            ProcessCount += 1
-            PID &= process("ProcessId")
+            processCount += 1
+            pid &= process("ProcessId")
         Next
         searcher = Nothing
         Return bAns
@@ -284,11 +291,11 @@ Public Class BSProcessInfo
     ''' </summary>
     ''' <param name="sProcessName"></param>
     ''' <returns>true/false</returns>
-    Public Function ExactProcessExists(sProcessName As String, Optional ByRef ProcessCount As Integer = 0) As Boolean
+    Public Function ExactProcessExists(sProcessName As String, Optional ByRef processCount As Integer = 0) As Boolean
         Dim bAns As Boolean = False
         Dim p() As Process
         p = Process.GetProcessesByName(sProcessName)
-        ProcessCount = p.Count
+        processCount = p.Count
         If p.Count > 0 Then
             bAns = True
         End If
@@ -299,11 +306,11 @@ Public Class BSProcessInfo
     ''' Use the Performance counter Process/Working Set - Private to get the 
     ''' memory that is used for a process
     ''' </summary>
-    ''' <param name="ProcessName"></param>
+    ''' <param name="processName"></param>
     ''' <returns>memory in bytes</returns>
-    Public Function GetProcessMemoryUseage(ProcessName As String) As String
+    Public Function GetProcessMemoryUseage(processName As String) As String
         Dim sAns As String = ""
-        Dim o As New System.Diagnostics.PerformanceCounter("Process", "Working Set - Private", ProcessName)
+        Dim o As New System.Diagnostics.PerformanceCounter("Process", "Working Set - Private", processName)
         sAns = o.RawValue
         o.Close()
         Return sAns
@@ -316,7 +323,7 @@ Public Class BSProcessInfo
     ''' <param name="OldValue"></param>
     ''' <param name="NewValue"></param>
     ''' <returns></returns>
-    Public Function GetProcessCPUTime(ProcessName As String, TIMER_INTERVAL As Long, OldValue As Double, ByRef NewValue As Double) As String
+    Public Function GetProcessCpuTime(ProcessName As String, TIMER_INTERVAL As Long, OldValue As Double, ByRef NewValue As Double) As String
         Dim sAns As String = ""
         Dim o As New System.Diagnostics.PerformanceCounter("Process", "% Processor Time", ProcessName)
         If OldValue = 0 Then
