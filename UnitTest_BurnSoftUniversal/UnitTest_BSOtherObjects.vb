b1a8f4b681aab49c82bd58398053912905781b2d
Joe Mireles <joe.mireles@atlasied.com>
2018-12-13 09:54:34 -0500

    #2 Created unit for class BSOtherObjects

---
+++ UnitTest_BurnSoftUniversal/UnitTest_BSOtherObjects.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+' ReSharper disable once InconsistentNaming
+<TestClass()> Public Class UnitTest_BSOtherObjects
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
