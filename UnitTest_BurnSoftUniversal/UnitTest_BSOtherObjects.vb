bb16ef43f5b9f8d5cbfad114a1710b0d377f04a8
Joe Mireles <joe.mireles@atlasied.com>
2018-12-13 10:03:19 -0500

    #2 added tests for functions in BSOtherObjects

--- UnitTest_BurnSoftUniversal/UnitTest_BSOtherObjects.vb
+++ UnitTest_BurnSoftUniversal/UnitTest_BSOtherObjects.vb
@@ -1,10 +1,31 @@
 ﻿Imports System.Text
 Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports BurnSoft.Universal
 
 ' ReSharper disable once InconsistentNaming
 <TestClass()> Public Class UnitTest_BSOtherObjects
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestMethod_StringCompairMatch()
+        Dim obj As New BSOtherObjects
+        Dim didPass As Boolean = obj.StringCompare("test","test")
+        If didPass Then 
+            Debug.Print("Strings Matched!")
+        Else 
+            Debug.Print("Strings Didn't Matched!")
+        End If
+        General.HasValue(didPass)
     End Sub
-
+    <TestMethod()> Public Sub TestMethod_StringCompairMisMatch()
+        Dim obj As New BSOtherObjects
+        Dim didPass As Boolean = obj.StringCompare("Test","test")
+        If didPass Then 
+            Debug.Print("Strings Matched!")
+        Else 
+            Debug.Print("Strings Didn't Matched!")
+            didPass = true
+        End If
+        General.HasValue(didPass)
+    End Sub
+    '<TestMethod()> Public Sub TestMethod_()
+    'End Sub
 End Class
