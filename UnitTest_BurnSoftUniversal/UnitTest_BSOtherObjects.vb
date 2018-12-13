fab3ca35461d3af86b2fb0fd38b4b686beb0af04
Joe Mireles <joe.mireles@atlasied.com>
2018-12-13 10:17:42 -0500

    #2 added tests for functions in BSOtherObjects

--- UnitTest_BurnSoftUniversal/UnitTest_BSOtherObjects.vb
+++ UnitTest_BurnSoftUniversal/UnitTest_BSOtherObjects.vb
@@ -26,6 +26,40 @@ Imports BurnSoft.Universal
         End If
         General.HasValue(didPass)
     End Sub
+    <TestMethod()> Public Sub TestMethod_ContentsExistsRegEx()
+        Dim obj As New BSOtherObjects
+        Dim didPass as Boolean = obj.ContentsExistsRegEx("This is a test, this is only a test","only")
+        If didPass Then 
+            Debug.Print("Keywork Found!")
+        Else 
+            Debug.Print("Didn't find key work in phrse")
+            didPass = true
+        End If
+        General.HasValue(didPass)
+    End Sub
+    <TestMethod()> Public Sub TestMethod_ArraysEqual()
+        Dim obj As New BSOtherObjects
+        Dim bytes(1000 * 1000 * 3 - 1) As Byte
+        Dim bytes2(1000 * 1000 * 3 - 1) As Byte
+        Dim didPass As Boolean = obj.ArraysEqual(bytes, bytes2)
+        If didPass Then 
+            Debug.Print("Strings Matched!")
+        Else 
+            Debug.Print("Strings Didn't Matched!")
+        End If
+        General.HasValue(didPass)
+    End Sub
+    <TestMethod()> Public Sub TestMethod_Pause()
+        Dim obj As New BSOtherObjects
+        Dim didPass As Boolean
+        Try
+            obj.Pause(100)
+            didPass = true
+        Catch ex As Exception
+            didPass = false
+        End Try
+        General.HasValue(didPass)
+    End Sub
     '<TestMethod()> Public Sub TestMethod_()
     'End Sub
 End Class
