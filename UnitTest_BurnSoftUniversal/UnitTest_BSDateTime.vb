972e929a25ab25c176f5ba75e94f79509dab3514
burnsoftnet <bs.github@burnsoft.net>
2018-12-14 15:45:20 -0500

    #2 working on unit tests

---
+++ UnitTest_BurnSoftUniversal/UnitTest_BSDateTime.vb
@@ -0,0 +1,10 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class UnitTest_BSDateTime
+
+    <TestMethod()> Public Sub TestMethod_ISDatePastNow()
+
+    End Sub
+
+End Class
