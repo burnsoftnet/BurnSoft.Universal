8011d2e822d7480b1b37304d55dd7c12429d8b21
Joe Mireles <joe.mireles@atlasied.com>
2018-12-17 11:38:28 -0500

    #2 created new empty unit test for FileIO

---
+++ UnitTest_BurnSoftUniversal/UnitTest_FileIO.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports BurnSoft.Universal
+<TestClass()> Public Class UnitTest_FileIO
+
+    <TestMethod()> Public Sub TestMethod1()
+    End Sub
+
+End Class
