04c1e76f8a1610961c35900c9ef78b393055ad01
burnsoftnet <bs.github@burnsoft.net>
2019-03-16 19:26:29 -0400

    #1 Updated comments

--- BurnSoft.Universal/BSEncryption.vb
+++ BurnSoft.Universal/BSEncryption.vb
@@ -4,17 +4,33 @@
 '-------------------------------------------
 Imports System.IO
 Imports System.Security.Cryptography
+''' <summary>
+''' Class BSEncryption.
+''' </summary>
 Public Class BSEncryption
+    ''' <summary>
+    ''' Enum SHATYPE
+    ''' </summary>
     Public Enum SHATYPE
         SHA1 = 1
         SHA256 = 256
         SHA384 = 384
         SHA515 = 512
     End Enum
+    ''' <summary>
+    ''' Gets the check sum io.
+    ''' </summary>
+    ''' <param name="sValue">The s value.</param>
+    ''' <returns>System.String.</returns>
     Function GetCheckSumIO(sValue As String) As String
         'Using Stream As Filestream = System.
         Return ""
     End Function
+    ''' <summary>
+    ''' Gets the check sum.
+    ''' </summary>
+    ''' <param name="file">The file.</param>
+    ''' <returns>System.String.</returns>
     Function GetCheckSum(file As String) As String
         Using stream As FileStream = System.IO.File.OpenRead(file)
             Dim sha As SHA1Managed = New SHA1Managed
@@ -22,6 +38,11 @@ Public Class BSEncryption
             Return BitConverter.ToString(checksum).Replace("-", String.Empty)
         End Using
     End Function
+    ''' <summary>
+    ''' Gets the check sum256.
+    ''' </summary>
+    ''' <param name="file">The file.</param>
+    ''' <returns>System.String.</returns>
     Function GetCheckSum256(file As String) As String
         Using stream As FileStream = System.IO.File.OpenRead(file)
             Dim sha As SHA256Managed = New SHA256Managed
@@ -29,6 +50,11 @@ Public Class BSEncryption
             Return BitConverter.ToString(checksum).Replace("-", String.Empty)
         End Using
     End Function
+    ''' <summary>
+    ''' Gets the check sum384.
+    ''' </summary>
+    ''' <param name="file">The file.</param>
+    ''' <returns>System.String.</returns>
     Function GetCheckSum384(file As String) As String
         Using stream As FileStream = System.IO.File.OpenRead(file)
             Dim sha As SHA384Managed = New SHA384Managed
@@ -36,6 +62,11 @@ Public Class BSEncryption
             Return BitConverter.ToString(checksum).Replace("-", String.Empty)
         End Using
     End Function
+    ''' <summary>
+    ''' Gets the check sum512.
+    ''' </summary>
+    ''' <param name="file">The file.</param>
+    ''' <returns>System.String.</returns>
     Function GetCheckSum512(file As String) As String
         Using stream As FileStream = System.IO.File.OpenRead(file)
             Dim sha As SHA512Managed = New SHA512Managed
@@ -43,6 +74,12 @@ Public Class BSEncryption
             Return BitConverter.ToString(checksum).Replace("-", String.Empty)
         End Using
     End Function
+    ''' <summary>
+    ''' Returns the sha check sum.
+    ''' </summary>
+    ''' <param name="sFile">The s file.</param>
+    ''' <param name="MySHAType">Type of my sha.</param>
+    ''' <returns>System.String.</returns>
     Public Function ReturnSHACheckSum(sFile As String, MySHAType As SHATYPE) As String
         Dim sAns As String = ""
         Select Case MySHAType
