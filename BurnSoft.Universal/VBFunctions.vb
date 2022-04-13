e584adf7c80702d81d333c30446282bcc8a25a28
Joe Mireles <jm.github@burnsoft.net>
2022-04-13 09:55:57 -0400

    #16 Added DateDiff

--- BurnSoft.Universal/VBFunctions.vb
+++ BurnSoft.Universal/VBFunctions.vb
@@ -3,11 +3,21 @@
 ''' </summary>
 Public Class VbFunctions
     ''' <summary>
-    ''' Checks to see if the Value is Numeric or not
+    ''' Checks to see if the Value is Numeric or not, uses vb function IsNumeric
     ''' </summary>
     ''' <param name="value">The value.</param>
     ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
     Public Shared Function ValueIsnumeric(value As Object) As Boolean
         Return IsNumeric(value)
+    End Function
+    ''' <summary>
+    ''' Gets the date difference betweek two dates, or uses vb function datediff
+    ''' </summary>
+    ''' <param name="startDate">The start date.</param>
+    ''' <param name="endDate">The end date.</param>
+    ''' <param name="interval">The interval.</param>
+    ''' <returns>System.Int64.</returns>
+    Public Shared Function GetDateDiff(startDate As Date, endDate As Date, interval As DateInterval) As Long
+        Return DateDiff(interval, startDate, endDate)
     End Function
 End Class
