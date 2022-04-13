08942275205e902487abcb929a9c58898fc1dcaf
Joe Mireles <jm.github@burnsoft.net>
2022-04-13 09:52:04 -0400

    #16 Added isnumeric function

--- BurnSoft.Universal/VBFunctions.vb
+++ BurnSoft.Universal/VBFunctions.vb
@@ -1,3 +1,13 @@
-﻿Public Class VBFunctions
-    'Public Shared Function ValueIsnumeric()
+﻿''' <summary>
+''' Functions that are availabel and make life easier in bn/vb.net but are not a part of c#
+''' </summary>
+Public Class VbFunctions
+    ''' <summary>
+    ''' Checks to see if the Value is Numeric or not
+    ''' </summary>
+    ''' <param name="value">The value.</param>
+    ''' <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+    Public Shared Function ValueIsnumeric(value As Object) As Boolean
+        Return IsNumeric(value)
+    End Function
 End Class
